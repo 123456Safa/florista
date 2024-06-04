79519a3273540b64f867668f000d47c939ca7d9f
123456Safa <166947428+123456Safa@users.noreply.github.com>
2024-06-04 10:02:19 +0100

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,39 @@
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="home.php">home</a>
+            <a href="about.php">about</a>
+            <a href="contact.php">contact</a>
+            <a href="shop.php">shop</a>
+        </div>
+
+        <div class="box">
+            <h3>extra links</h3>
+            <a href="login.php">login</a>
+            <a href="register.php">register</a>
+            <a href="orders.php">my orders</a>
+            <a href="cart.php">my cart</a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <p> <i class="fas fa-phone"></i> +216 88-88-88-88 </p>//phone icon
+            <p> <i class="fas fa-phone"></i> +216 11-22-33-44 </p> //phone icon
+            <p> <i class="fas fa-envelope"></i> 2émebBI@gmail.com </p>/envelope icon
+            <p> <i class="fas fa-map-marker-alt"></i> Tunis,faculté des sciences économiques et de gestion  </p> //map icon
+        </div>
+
+        <div class="box">
+            <h3>follow us</h3>
+            <a href="#"><i class="fab fa-facebook-f"></i>facebook</a> //facebook icon
+            <a href="#"><i class="fab fa-instagram"></i>instagram</a> //instagram icon
+            <a href="#"><i class="fab fa-snapchat"></i>snapchat</a> //snapchat icon
+        </div>
+
+    </div>
+
+    <div class="credit">&copy; copyright @ <?php echo date('Y'); ?> by <span>2éme BI</span> </div> 
+</section>
